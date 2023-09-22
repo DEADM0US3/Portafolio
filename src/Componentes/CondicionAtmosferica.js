ed10cb90f8dac26afc2ded3527582048260d300b
kevin-alexis <kevinlevin367@outlook.com>
2023-09-22 13:32:27 -0500

    Add blur background

--- src/Componentes/CondicionAtmosferica.js
+++ src/Componentes/CondicionAtmosferica.js
@@ -4,48 +4,56 @@ import './CondicionAtmosferica.css'
 function CondicionAtmosferica() {
     const url = "https://api.datos.gob.mx/v1/condiciones-atmosfericas";
     const estadosMx = [
-        { id: 1, name: "Aguascalientes" },
-        { id: 2, name: "Baja California" },
-        { id: 3, name: "Baja California Sur" },
-        { id: 4, name: "Campeche" },
-        { id: 5, name: "Chiapas" },
-        { id: 6, name: "Chihuahua" },
-        { id: 7, name: "Ciudad de México" },
-        { id: 8, name: "Coahuila" },
-        { id: 9, name: "Colima" },
-        { id: 10, name: "Durango" },
-        { id: 11, name: "Estado de México" },
-        { id: 12, name: "Guanajuato" },
-        { id: 13, name: "Guerrero" },
-        { id: 14, name: "Hidalgo" },
-        { id: 15, name: "Jalisco" },
-        { id: 16, name: "Michoacán" },
-        { id: 17, name: "Morelos" },
-        { id: 18, name: "Nayarit" },
-        { id: 19, name: "Nuevo León" },
-        { id: 20, name: "Oaxaca" },
-        { id: 21, name: "Puebla" },
-        { id: 22, name: "Queretaro" },
-        { id: 23, name: "Quintana Roo" },
-        { id: 24, name: "San Luis Potosi" },
-        { id: 25, name: "Sinaloa" },
-        { id: 26, name: "Sonora" },
-        { id: 27, name: "Tabasco" },
-        { id: 28, name: "Tamaulipas" },
-        { id: 29, name: "Tlaxcala" },
-        { id: 30, name: "Veracruz" },
-        { id: 31, name: "Yucatan" },
-        { id: 32, name: "Zacatecas" }];
-        
-    const [datos, setDatos] = useState([]);
-    const [estadoActual, setEstadoActual] = useState("Quintana Roo")
+                { id: 1, name: "Aguascalientes" },
+                { id: 2, name: "Baja California" },
+                { id: 3, name: "Baja California Sur" },
+                { id: 4, name: "Campeche" },
+                { id: 5, name: "Chiapas" },
+                { id: 6, name: "Chihuahua" },
+                { id: 7, name: "Ciudad de Mexico" },
+                { id: 8, name: "Coahuila" },
+                { id: 9, name: "Colima" },
+                { id: 10, name: "Durango" },
+                { id: 11, name: "Estado de Mexico" },
+                { id: 12, name: "Guanajuato" },
+                { id: 13, name: "Guerrero" },
+                { id: 14, name: "Hidalgo" },
+                { id: 15, name: "Jalisco" },
+                { id: 16, name: "Michoacan" },
+                { id: 17, name: "Morelos" },
+                { id: 18, name: "Nayarit" },
+                { id: 19, name: "Nuevo Leon" },
+                { id: 20, name: "Oaxaca" },
+                { id: 21, name: "Puebla" },
+                { id: 22, name: "Queretaro" },
+                { id: 23, name: "Quintana Roo" },
+                { id: 24, name: "San Luis Potosi" },
+                { id: 25, name: "Sinaloa" },
+                { id: 26, name: "Sonora" },
+                { id: 27, name: "Tabasco" },
+                { id: 28, name: "Tamaulipas" },
+                { id: 29, name: "Tlaxcala" },
+                { id: 30, name: "Veracruz" },
+                { id: 31, name: "Yucatan" },
+                { id: 32, name: "Zacatecas" }];
 
+    const [datos, setDatos] = useState([]);
+    const [estadoActual, setEstadoActual] = useState("");
+    const [cargando, setCargando] = useState(false);
 
     const consultarDatos = () => {
-        // return fetch(url)
-        return fetch(`${url}?state=${estadoActual}`)
+        setCargando(true); // Mostrar el mensaje de carga
+
+        fetch(`${url}?state=${estadoActual}`)
             .then((res) => res.json())
-            .then((condicionAtm) => setDatos(condicionAtm.results))
+            .then((condicionAtm) => {
+                setDatos(condicionAtm.results);
+                setCargando(false); // Ocultar el mensaje de carga cuando los datos se carguen
+            })
+            .catch((error) => {
+                console.error("Error al obtener datos:", error);
+                setCargando(false); // Ocultar el mensaje de carga en caso de error
+            });
     }
 
     useEffect(() => {
@@ -55,38 +63,39 @@ function CondicionAtmosferica() {
     return (
         <>
             <div>
-                <h1> Estado del Tiempo</h1>
+                <h1>Estado del Tiempo</h1>
             </div>
             <select className="selectState"
                 onChange={(e) => setEstadoActual(e.target.value)}
             >
-                <option value="">Selecciona una opción</option> 
+                <option value="">Selecciona una opción</option>
                 {estadosMx.map((opcion) => (
                     <option key={opcion.id} value={opcion.name}>
                         {opcion.name}
                     </option>
                 ))}
             </select>
-            {/* {estadoActual} */}
-
             
-            <div className="container-cards">
-            
-            
-            {datos.map((ciudad, index) => {
-                    if (ciudad.state == estadoActual  && index<=8) {
-                        return (
-                            <div key={index} className="card">
-                                <h6>Ciudad: {ciudad.name}</h6>
-                                <p> <i>Estado del clima: {ciudad.skydescriptionlong}</i></p>
-                                <p> <i>Temperatura: {ciudad.tempc}°</i></p>
-                            </div>
-            
-                        );
-                    }
-                })}
-            </div>
+            {cargando ? (
+                <p>Cargando datos...</p> // Mensaje de carga
+            ) : (
+                <div className="container-cards">
+                    {datos.map((ciudad, index) => {
+                        if (ciudad.state === estadoActual && index <= 8) {
+                            return (
+                                <div key={index} className="card">
+                                    <h6>Ciudad: {ciudad.name}</h6>
+                                    <p><i>Estado del clima: {ciudad.skydescriptionlong}</i></p>
+                                    <p><i>Temperatura: {ciudad.tempc}°</i></p>
+                                </div>
+                            );
+                        }
+                        return null; // Agregar un return null para evitar problemas con elementos vacíos
+                    })}
+                </div>
+            )}
         </>
     );
 }
+
 export default CondicionAtmosferica;
